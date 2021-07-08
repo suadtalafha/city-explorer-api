95a6c97aa98d33e2e2bfdcd62ff20ddb0b6fe63e
suadtalafha <suadhusam@yahoo.com>
2021-07-08 18:41:52 +0300

    save

--- modules/movie.js
+++ modules/movie.js
@@ -1,10 +1,14 @@
 'use strict';
 const axios=require('axios');
+let inMemory={};
 function getMovie(request, respons) {
     const cQuery = request.query.cityName;
     let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cQuery}`
-
-    axios.get(url)
+    if(inMemory[cQuery] !== undefined){
+        console.log('we got the data from our server');
+        respons.send(inMemory[cQuery]);
+    }else{
+        axios.get(url)
        
         .then(moveiData => {
             respons.status(200).send(moveiData.data.results.map(val => {
@@ -15,6 +19,8 @@ function getMovie(request, respons) {
         })
 
 }
+    }
+    
 
 class Movei{
     constructor(val){
